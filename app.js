6d6dcff1f6ae8a5af14b57a6b9d6de90e43705f9
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-12-29 22:43:36 +0630

    #add: handling unhandled routes

--- app.js
+++ app.js
@@ -6,4 +6,12 @@ const blogRouter = require("./routers/blogRouter");
 const app = express();
 
 app.use("/api/blogs", blogRouter);
+
+// handling unhandled routes
+app.all("*", (req, res) => {
+    res.status(404).json({
+        status: "fail",
+        message: "Page not found ⚠",
+    });
+});
 module.exports = app;
